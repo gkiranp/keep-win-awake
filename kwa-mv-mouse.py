03f794033d4b81802ea37c578bfe0372c5dbe901
Puranik, KiranX <kiranx.puranik@intel.com>
2021-03-25 13:47:56 +0800

    Modified to run in a thread and better operation handling

--- kwa-mv-mouse.py
+++ kwa-mv-mouse.py
@@ -1,62 +1,81 @@
-import win32api, win32con
-from win32api import GetSystemMetrics
+import win32api as w32api
+import win32con as w32con
+from win32api import GetSystemMetrics as gsm
+import threading
 import time
 
-def about():
-	print("-----------------------------x---------------------------------")
-	print("If you like this script, send me a hi message here: ")
-	print("Email: gkiranp@gmail.com")
 
-def usage():
-	print("-----------------------------x---------------------------------")
-	print("Usage:")
-	print("Usage is simple, just run the script in Win machine")
-	print("Requirement is : Python 3")
-	print("Tested in Windows 10, 64-bit")
-	print("Close the window to close the script, or Ctrl+C must do")
+class MoveMouse (threading.Thread):
+	ExitFlag = False
+	Timeout = 30
 
-def lic():
-	print("-----------------------------x---------------------------------")
-	print("LICENSE: MIT")
-	print("Permission is hereby granted, free of charge, to any person" )
-	print("obtaining a copy of this software and associated documentation")
-	print("files, to deal in the Software without restriction, including ")
-	print("without limitation the rights to use, copy, modify, merge, publish, ")
-	print("distribute, sublicense, and/or sell copies of the Software, and to ")
-	print("permit persons to whom the Software is furnished to do so, subject ")
-	print("to the below` conditions:")
-	print("THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS ")
-	print("OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,")
-	print("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE")
-	print("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER ")
-	print("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, ")
-	print("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR ")
-	print("OTHER DEALINGS IN THE SOFTWARE.")
-	print("-----------------------------x---------------------------------")
+	def disp(self, msg:str):
+		print ('{:^24s}'.format(msg))
 
-def click(x,y):
-	win32api.SetCursorPos((x,y))
-	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+	def about(self):
+		self.disp("-----------------------------x---------------------------------")
+		self.disp("If you like this script, send me a Hi Email")
+		self.disp("Email: gkiranp@gmail.com")
 
-# Script stars here
-about()
-usage()
-lic()
-# Define start Row,Col
-r = 10
-c = 10
-print("Current screen resolution = " + str(GetSystemMetrics(0)) + "x" + str(GetSystemMetrics(1)))
-print("Auto mouse movement started:")
-# Let's loop infinitely
-while True:
-	r = r + 10
-	c = c + 10
-	# Make sure you are not out of bound from current display resolution
-	if(r >= GetSystemMetrics(0)):
-		r = 10
-	# Make sure you are not out of bound from current display resolution
-	if(c >= GetSystemMetrics(1)):
-		c = 10
-	click(r,c)
-	time.sleep(15)
+	def usage(self):
+		self.disp("-----------------------------x---------------------------------")
+		self.disp("Usage:")
+		self.disp("Run the script in a Windows machine, where Python 3 is installed.")
+		self.disp("Close the window to close the script, or Ctrl+C must do.")
+
+	def license(self):
+		self.disp("-----------------------------x---------------------------------")
+		self.disp("LICENSE: MIT")
+		self.disp("Permission is hereby granted, free of charge, to any person" )
+		self.disp("obtaining a copy of this software and associated documentation")
+		self.disp("files, to deal in the Software without restriction, including ")
+		self.disp("without limitation the rights to use, copy, modify, merge, publish, ")
+		self.disp("distribute, sublicense, and/or sell copies of the Software, and to ")
+		self.disp("permit persons to whom the Software is furnished to do so, subject ")
+		self.disp("to the below` conditions:")
+		self.disp("THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS ")
+		self.disp("OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,")
+		self.disp("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE")
+		self.disp("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER ")
+		self.disp("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, ")
+		self.disp("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR ")
+		self.disp("OTHER DEALINGS IN THE SOFTWARE.")
+		self.disp("-----------------------------x---------------------------------")
+
+	def click(self, x, y):
+		w32api.SetCursorPos((x,y))
+		w32api.mouse_event(w32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+		w32api.mouse_event(w32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+	
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.about()
+		self.license()
+		self.usage()
+	
+	def run(self):
+		r = int((gsm(0)/2) - 10)
+		c = int((gsm(1)/2) - 10)
+		while not self.ExitFlag:
+			if self.Timeout == 0:
+				if(r >= int(gsm(0)) or c >= int(gsm(1))):
+					r = int((gsm(0)/2) - 10)
+					c = int((gsm(1)/2) - 10)
+				self.click(r,c)
+				self.Timeout = 30
+			else:
+				self.Timeout -= 1
+			time.sleep(0.5)
+
+# Main program
+if __name__ == "__main__":
+	th = MoveMouse()
+	th.start()
+	# Let's keep main thread alive
+	while th.is_alive():
+		try:
+			# synchronization timeout of threads kill
+			th.join(1)
+		except KeyboardInterrupt:
+			# Ctrl-C handling and send kill to threads
+			th.ExitFlag = True
